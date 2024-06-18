07efd59dffe870211f0b9738618869d9e83eb760
hheeju98 <hheeju98@naver.com>
2024-06-18 23:38:00 +0900

    coding challenge2

--- script.js
+++ script.js
@@ -284,3 +284,21 @@ const max = movements.reduce((acc, mov) => {
         return mov;
 }, movements[0]);
 console.log(max);
+
+
+const calAverageHumanAge = function (ages) {
+    const humanAges = ages.map(age => age <= 2 ? 2 * age : 16 + age * 4);
+    console.log(humanAges);
+    const adults = humanAges.filter(age => age >= 18);
+    console.log(adults);
+    console.log(humanAges);
+
+    // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+    const average = adults.reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+    return average;
+}
+const avg1 = calAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
